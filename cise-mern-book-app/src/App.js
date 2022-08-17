807ea220f112c7d46e8cfc24024cd342c1dc752c
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2022-08-17 15:40:35 +1200

    Donwgraded react-router-dom to 5.2.0

--- cise-mern-book-app/src/App.js
+++ cise-mern-book-app/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Route exact path='/' element={<ShowBookList/>} />
+          <Route exact path='/' component={ShowBookList} />
           <Route path='/create-book' component={CreateBook} />
           <Route path='/edit-book/:id' component={UpdateBookInfo} />
           <Route path='/show-book/:id' component={ShowBookDetails} />
