7a07043a70cda7d6799af4dc6afc751381f2cfaa
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2022-08-17 21:05:29 +1200

    Updated env files

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get('/', (req, res) => res.send('Hello world!'));
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
