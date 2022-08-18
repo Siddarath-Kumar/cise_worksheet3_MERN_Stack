42a4a7819ff7d339790482db6e5c406f05c580ba
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2022-08-18 18:57:16 +1200

    Make changes to .env, app.js and package.json following the tutorial

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-// app.js
-
+require("dotenv").config({ path: "./env"});
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
-
-// routes
 const books = require('./routes/api/books');
-
 const app = express();
+const path = require('path');
+const { response } = require('express');
 
-// Connect Database
 connectDB();
 
-// cors
 app.use(cors({ origin: true, credentials: true }));
-
-// Init Middleware
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-// use Routes
+//app.get('/', (req, res) => res.send('Hello world!'));
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(express.static(path.join(__dirname, "./cise-mern-book-app/build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "cise-mern-book-app", "build", "index.html"))
+})
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
